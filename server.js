163fa7a6d3c428feb6fe82a93f7e9bf9a9c5ed1b
John Joo <john@Johns-iMac.local>
2018-05-24 18:40:42 -0700

    added server.js and schemas

--- server.js
+++ server.js
@@ -1,44 +1,138 @@
 const express = require('express');
 
-const app = express();
-
 const blogPostRouter = require('./blogpostrouter');
 
-app.use('/blog-posts', blogPostRouter); 
+const mongoose = require('mongoose'); 
 
-// both runServer and closeServer need to access the same
-// server object, so we declare `server` here, and then when
-// runServer runs, it assigns a value.
-let server;
+mongoose.Promise = global.Promise;
 
-// this function starts our server and returns a Promise.
-// In our test code, we need a way of asynchronously starting
-// our server, since we'll be dealing with promises there.
-function runServer() {
-  const port = process.env.PORT || 8080;
+
+const {PORT, DATABASE_URL} = require('./config');
+const {BlogPost} = require('./models');
+
+const app = express(); 
+
+app.use(morgan('common')); 
+app.use(express.json()); 
+
+app.get('/posts', (req, res) => {
+  BlogPost
+    .find()
+    .then(posts => { 
+      res.jason(posts.map(post => post.serialize()));
+    })
+    .catch(err => { 
+      console.error(err);
+      res.status(500).json({error: 'something went terribly wrong'}); 
+    });
+}); 
+
+app.get('/posts/:id',(req, res) => {
+  BlogPost  
+    .findById(req.params.id)
+    .then(post => res.json(post.serialize()))
+    .catch(err => { 
+      console.error(err); 
+      res.status(500).json({error: 'something went horribly awry'}); 
+    });
+});
+
+app.post('/posts', (req, res) => {
+  const requiredFields = ['title', 'content', 'author']; 
+  for (let i = 0; i < requiredFields.length; i++) { 
+    const field = requiredFields[i]; 
+    if (!(field in req.body)) { 
+      const message = 'Missing \`${field}` in request body';
+      console.error(message); 
+      return res.status(400).send(message);  
+    }
+  }
+
+    Blogpost 
+      .create({
+        title: req.body.title, 
+        content: req.body.content, 
+        author: req.body.author
+      })
+      //lower case blogPost an argument created from above? 
+      .then(blogPost => res.status(201).json(blogPost.serialize()))
+      .catch(err => {
+        console.error(err); 
+        res.status(500).json({ error: 'Something went wrong' });
+      });
+}); 
+
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id must match'
+    });
+  }
+
+  const updated = {}; 
+  const updateableFields = ['title', 'content', 'author'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      update[field] = req.body[field]; 
+    }
+  });
+
+  BlogPost 
+    .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+    .then(updatedPost => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Something went wrong '})); 
+}); 
+
+app.delete('/:id', (req, res) => {
+  BlogPost  
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      console.log(`Deleted blog post with id \`${req.params.id}\``); 
+      res.status(204).end(); 
+    });
+});
+
+//what is this? 
+app.use('*', function (req, res) { 
+  res.status(404).json({ message: 'Not Found'}); 
+}); 
+
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server; 
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl, port = PORT) { 
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`); 
+        resolve(); 
+      })
+        .on('error', err => {
+          mongoose.disconnect(); 
+          reject(err);
+        }); 
     });
   });
 }
 
-// like `runServer`, this function also needs to return a promise.
-// `server.close` does not return a promise on its own, so we manually
-// create one.
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        // so we don't also call `resolve()`
-        return;
-      }
-      resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server'); 
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(); 
+      });
     });
   });
 }
@@ -46,7 +140,7 @@ function closeServer() {
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
 
-module.exports = {app, runServer, closeServer};
+module.exports = { runServer, app, closeServer };
